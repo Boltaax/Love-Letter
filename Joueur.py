312adddccd7618ae49fac916c5291a0af31db3b2
Lelestar <leonard.zipper68@gmail.com>
2023-11-15 11:58:35 +0100

    Start finishing game

--- Joueur.py
+++ Joueur.py
@@ -1,13 +1,13 @@
 import random
 
-class Joueur:
-    def __init__(self,nom):
-        self.nom = nom
+class Player:
+    def __init__(self, name):
+        self.name = name
         self.hand = []
         self.reachable = True
 
-    def piocher(self, carte):
-        self.hand.append(carte)
+    def draw(self, card):
+        self.hand.append(card)
 
     def play_best_card(self):
         return self.hand.pop(random.randint(0, 1))
@@ -16,15 +16,15 @@ class Joueur:
         return self.hand[0]
 
     def throw(self):
-        carte = self.hand.pop()
-        #   print(self.nom, " avait ",carte.nom, " dans sa main !" )
-        return carte
+        card = self.hand.pop()
+        #   print(self.name, " had ", card.name, " in their hand!")
+        return card
 
-    def choose_card_to_keep(self, cartes):
-        return cartes[random.randint(0, len(cartes)-1)]
+    def choose_card_to_keep(self, cards):
+        return cards[random.randint(0, len(cards)-1)]
 
     def comtesse(self):
-        # Vérifiez si le joueur a la carte "Comtesse" et une carte "Roi" ou "Prince" dans sa main
-        has_comtesse = any(carte.nom == "Comtesse" for carte in self.hand)
-        has_king_or_prince = any(carte.nom in ["Roi", "Prince"] for carte in self.hand)
+        # Check if the player has the "Comtesse" card and a "King" or "Prince" card in their hand
+        has_comtesse = any(card.name == "Comtesse" for card in self.hand)
+        has_king_or_prince = any(card.name in ["King", "Prince"] for card in self.hand)
         return has_comtesse and has_king_or_prince
