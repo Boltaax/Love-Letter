e114d78413b12562a4e131eaeb805f4c9f15665c
Lelestar <leonard.zipper68@gmail.com>
2023-11-16 20:36:25 +0100

    Refactor code to improve performance and readability.

--- Game.py
+++ Game.py
@@ -94,24 +94,24 @@ class LoveLetterGame:
         target_player = self.choose_target_player()
         if target_player is not None:
             guessed_character = self.choose_character()
-            #   print(f"{self.players[self.active_player].name} chooses {target_player.name} as target and guesses {guessed_character}")
+            print(f"{self.players[self.active_player].name} chooses {target_player.name} as target and guesses {guessed_character}")
             if guessed_character in [c.name for c in target_player.hand]:
-                #   print("The guess is correct. The target player is eliminated from the round.")
+                print("The guess is correct. The target player is eliminated from the round.")
                 target_player.hand = []
             else:
-                #   print("The guess is incorrect. Nothing happens.")
+                print("The guess is incorrect. Nothing happens.")
                 pass
         else :
-            #   print("No player is targetable, the card has no effect!")
+            print("No player is targetable, the card has no effect!")
             pass
 
     def effect_priest(self):
         target_player = self.choose_target_player()
         if target_player is not None:
-            #   print(f"{self.players[self.active_player].name} chooses {target_player.name} as target and looks at {target_player.card().name}")
+            print(f"{self.players[self.active_player].name} chooses {target_player.name} as target and looks at {target_player.card().name}")
             pass
         else :
-            #   print("No player is targetable, the card has no effect!")
+            print("No player is targetable, the card has no effect!")
             pass
 
     def effect_baron(self):
@@ -120,40 +120,41 @@ class LoveLetterGame:
             current_player = self.players[self.active_player]
             current_player_card = current_player.card()
             target_player_card = target_player.card()
-            #   print(f"{current_player.name} chooses {target_player.name} as target.")
-            #   print(f"{current_player.name} has the {current_player_card.name} card ({current_player_card.value}).")
-            #   print(f"{target_player.name} has the {target_player_card.name} card ({target_player_card.value}).")
+            print(f"{current_player.name} chooses {target_player.name} as target.")
+            print(f"{current_player.name} has the {current_player_card.name} card ({current_player_card.value}).")
+            print(f"{target_player.name} has the {target_player_card.name} card ({target_player_card.value}).")
             if current_player_card.value > target_player_card.value:
-                #   print(f"{current_player.name} wins the duel. {target_player.name} loses the round.")
-                target_player.hand.remove(target_player_card)
+                print(f"{current_player.name} wins the duel. {target_player.name} loses the round.")
+                target_player.hand = []
             elif current_player_card.value < target_player_card.value:
-                #   print(f"{target_player.name} wins the duel. {current_player.name} loses the round.")
-                current_player.hand.remove(current_player_card)
+                print(f"{target_player.name} wins the duel. {current_player.name} loses the round.")
+                current_player.hand = []
             else:
-                #   print("Tie, no one loses the round.")
+                print("Tie, no one loses the round.")
                 pass
         else :
-            #   print("No player is targetable, the card has no effect!")
+            print("No player is targetable, the card has no effect!")
             pass
 
     def effect_handmaid(self):
         self.players[self.active_player].reachable = False
-        #   print(f"{self.players[self.active_player].name} is not targetable for a turn!")
+        print(f"{self.players[self.active_player].name} is not targetable for a turn!")
 
     def effect_prince(self):
         target_player = self.choose_target_player()
         if target_player is not None and len(self.deck.draw_pile) > 0:
-            #   print(f"{self.players[self.active_player].name} chooses {target_player.name}")
+            print(f"{self.players[self.active_player].name} chooses {target_player.name}")
             discarded_card = target_player.discard()
             # if the discarded card is a spy, set has_played_or_discarded_spy to True
             if discarded_card.name == "Spy":
+                print(f"{target_player.name} discarded the Spy")
                 target_player.has_played_or_discarded_spy = True
             elif discarded_card.name == "Princess": # if the discarded card is a princess, the player is eliminated
-                #   print(f"{target_player.name} discarded the Princess and is eliminated from the round.")
+                print(f"{target_player.name} discarded the Princess and is eliminated from the round.")
                 target_player.hand = []
             target_player.draw(self.deck.draw())
         else :
-            #   print("No player is targetable, the card has no effect!")
+            print("No player is targetable, the card has no effect!")
             pass
 
     def effect_chancellor(self):
@@ -164,11 +165,11 @@ class LoveLetterGame:
             drawn_cards = [self.deck.draw(), self.deck.draw()]
 
         # Display the drawn cards.
-        #   print(f"{current_player.name} draws {len(drawn_cards)} cards: {[card.name for card in drawn_cards]}")
+        print(f"{current_player.name} draws {len(drawn_cards)} card(s): {[card.name for card in drawn_cards]}")
         # Check if any cards were drawn before continuing.
         if len(drawn_cards) > 0:
             card_to_keep = current_player.choose_card_to_keep(drawn_cards)
-            #   print(f"{current_player.name} keeps the {card_to_keep.name} card ({card_to_keep.value}).")
+            print(f"{current_player.name} keeps the {card_to_keep.name} card ({card_to_keep.value}).")
             cards_to_return = [card for card in drawn_cards if card != card_to_keep]
             self.deck.draw_pile.extend(cards_to_return)
 
@@ -176,18 +177,18 @@ class LoveLetterGame:
         target_player = self.choose_target_player()
         if target_player is not None:
             current_player = self.players[self.active_player]
-            #   print(f"{current_player.name} chooses {target_player.name} as target.")
+            print(f"{current_player.name} chooses {target_player.name} as target.")
             current_player_card = current_player.card()
             target_player_card = target_player.card()
-            #   print(f"{current_player.name} has the {current_player_card.name} card ({current_player_card.value}).")
-            #   print(f"{target_player.name} has the {target_player_card.name} card ({target_player_card.value}).")
+            print(f"{current_player.name} has the {current_player_card.name} card ({current_player_card.value}).")
+            print(f"{target_player.name} has the {target_player_card.name} card ({target_player_card.value}).")
             current_player.hand.remove(current_player_card)
             target_player.hand.remove(target_player_card)
             current_player.hand.append(target_player_card)
             target_player.hand.append(current_player_card)
-            #   print(f"{current_player.name} and {target_player.name} have exchanged their cards.")
+            print(f"{current_player.name} and {target_player.name} have exchanged their cards.")
         else :
-            #   print("No player is targetable, the card has no effect!")
+            print("No player is targetable, the card has no effect!")
             pass
 
     def effect_countess(self):
@@ -195,7 +196,7 @@ class LoveLetterGame:
 
     def effect_princess(self):
         current_player = self.players[self.active_player]
-        #   print(f"{current_player.name} plays the Princess and is eliminated from the round.")
+        print(f"{current_player.name} plays the Princess and is eliminated from the round.")
         current_player.hand = []
 
     def choose_target_player(self):
@@ -218,15 +219,22 @@ class LoveLetterGame:
         if len(self.deck.draw_pile) == 0 or (len([j for j in self.players if len(j.hand) > 0]) == 1):
             players_in_game = [j for j in self.players if len(j.hand) > 0]
             if players_in_game:
-                winning_player = max(players_in_game, key=lambda j: max([c.value for c in j.hand]))
-                self.points[winning_player] += 1
-                # ad spy bonus if the spy was play or discarded by only one player
-                spy_count = sum([1 for j in self.players if j.has_played_or_discarded_spy])
-                if spy_count == 1:
-                    benefit_player = [j for j in self.players if j.has_played_or_discarded_spy][0]
-                    self.points[benefit_player] += 1
-                #   print(f"{winning_player.name} wins the round and gets 1 point.")
-            self.new_round()
+                max_card_value = max([max([c.value for c in j.hand]) for j in players_in_game])
+                winning_players = [j for j in players_in_game if max([c.value for c in j.hand]) == max_card_value]
+                if len(winning_players) == 1:
+                    print(f"{winning_players[0].name} wins the round and gets 1 point!")
+                else:
+                    print(f"{', '.join([j.name for j in winning_players])} win the round and get 1 point each!")
+                for winning_player in winning_players:
+                    self.points[winning_player] += 1
+                    # add spy bonus if the spy was play or discarded by only one player
+                    spy_count = sum([1 for j in self.players if j.has_played_or_discarded_spy])
+                    if spy_count == 1:
+                        benefit_player = [j for j in self.players if j.has_played_or_discarded_spy][0]
+                        self.points[benefit_player] += 1
+                        print(f"{benefit_player.name} gets 1 bonus point for being the only player to play or discard a Spy card.")
+                print(f"Current points: {[f'{j.name}: {self.points[j]}' for j in self.points]}")
+                self.new_round()
 
     def new_round(self):
         self.deck = Deck()
